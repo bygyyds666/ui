a1dd70eddd3274e9a0fa666ff646c43d0b825faf
bygyyds666 <3967418825@qq.com>
2025-08-20 20:18:48 +0800

    Update 白名单.lua

--- 白名单.lua
+++ 白名单.lua
@@ -1,6 +1,9 @@
-    "91789478",
-    "hsjsjszbnsjwjz",
+return {
+    Users = {
+        91789478",
     "dchdhhfhxb",
     "gouchaoxi2",     --添加白名单的账号
     "Chinese1522",
-    "samcZSZ"
+    "samcZSZ",
+    }
+}
