2bee234d92a1aaa549220c35b00cc037791a27b7
bygyyds666 <3967418825@qq.com>
2025-08-21 07:49:43 +0800

    Update 白名单.lua

--- 白名单.lua
+++ 白名单.lua
@@ -1,11 +1,1 @@
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))() 
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))() 
--- 增强的白名单系统
-local WhitelistEnabled = true -- 是否启用白名单系统
-local Whitelist = {            
-    "91789478",
-    "dchdhhfhxb",
-    "gouchaoxi2",     --添加白名单的账号
-    "Chinese1522",
-    "samcZSZ"
-}
+
