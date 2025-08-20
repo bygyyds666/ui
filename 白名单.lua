17185d76bd40b5eedfe40a211dc8ae27d2653175
bygyyds666 <3967418825@qq.com>
2025-08-20 18:43:33 +0800

    Create 白名单.lua

---
+++ 白名单.lua
@@ -0,0 +1,9 @@
+local WhitelistEnabled = true -- 是否启用白名单系统
+local Whitelist = {            
+    "91789478",
+    "hsjsjszbnsjwjz",
+    "dchdhhfhxb",
+    "gouchaoxi2",     --添加白名单的账号
+    "Chinese1522",
+    "samcZSZ"
+}
